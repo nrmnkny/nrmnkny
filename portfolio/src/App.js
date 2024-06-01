72b6995edae30fafda1c528ee0d5b741973a55bd
nrmnkny <normane7@icloud.com>
2024-06-01 17:57:13 +0300

    blog v1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -36,7 +36,7 @@ const App = () => {
                 location="Nairobi"
                 email="normankenya0@gmail.com"
                 phone="(254) 705-532-531"
-                blogLink="/blog"
+                blogLink="https://blog.nrmnkny.vercel.app"
                 twitterLink="https://twitter.com/nrmnkny"
               />
               <div className="flex flex-grow">
