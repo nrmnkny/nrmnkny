3114fef3ad5cbea3eac1e4b950852b5f5cdbd4f8
nrmnkny <normane7@icloud.com>
2024-06-02 09:55:37 +0300

    hrk add

--- svr/server.js
+++ svr/server.js
@@ -7,7 +7,12 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors());
+// Allow requests from specific origins
+app.use(cors({
+  origin: ['https://nrmnkny.vercel.app', 'https://nrmnkny-74d777c56ce9.herokuapp.com', 'http://localhost:3000'], // Add your frontend domain and local development
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api/portfolio', dataRoutes);
