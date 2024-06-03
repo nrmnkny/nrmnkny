63b1adcdcd204f5fe213efad093cd046b2a62f29
nrmnkny <normane7@icloud.com>
2024-06-03 13:48:41 +0300

    new amRDZ

--- svr/routes/blog.js
+++ svr/routes/blog.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/', BlogController.getAllPosts);
-router.get('/:id', BlogController.getPostById); // Fetch a single post by ID
+router.get('/:id', BlogController.getPostById); 
 router.post('/new', authMiddleware, BlogController.createPost);
 router.put('/:id', authMiddleware, BlogController.updatePost);
 router.delete('/:id', authMiddleware, BlogController.deletePost);
--- svr/server.js
+++ svr/server.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors({
-  origin: ['https://nrmnkny.vercel.app'],
+  origin: ['localhost:3000/api'],
   credentials: true,
 }));
 
