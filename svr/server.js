59d11ec674ab07e78390c4b528266c6f5f680a08
nrmnkny <normane7@icloud.com>
2024-06-07 11:57:22 +0300

    fixed CORS GPPD methods

--- svr/server.js
+++ svr/server.js
@@ -3,18 +3,21 @@ const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes');
 const blogRoutes = require('./routes/blogRoutes');
 const authRoutes = require('./routes/authRoutes');
+const { checkAuth } = require('./middlewares/authMiddleware');
 
 const app = express();
 
 app.use(cors({
   origin: ['https://nrmnkny.vercel.app', 'https://nrmnkny-r6fc6b59h-quirkscodes-projects.vercel.app'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
 
 app.use('/api/portfolio', dataRoutes);
-app.use('/api/blog', blogRoutes);
+app.use('/api/blog', checkAuth, blogRoutes); 
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
